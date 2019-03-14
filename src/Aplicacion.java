cd186b97e1747c74e0af4d1b16668385245bc482
bp0418 <francisco.go.prieto@gmail.com>
2019-03-14 20:27:22 +0100

    2.3

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -3,6 +3,27 @@ public class Aplicacion {
 
     public static void main(String[] args) {
 
+        Paciente p1 = new Paciente("Alberto","Frecuentes mareos");
+        Paciente p2 = new Paciente("Ana","Infeccion resistente");
+        Paciente p3 = new Paciente("Eva", "Problemas digestivo");
+        Paciente p4 = new Paciente("Ernesto", "Problemas cardiovasculares");
 
+        ListaOrdinal Pacientes = new ListaOrdinal();
+
+        Pacientes.insertar(p1);
+        Pacientes.insertar(p2);
+        Pacientes.insertar(p3);
+        Pacientes.insertar(p4);
+
+        p2.darAlta();
+        p3.darAlta();
+
+        Iterador I = Pacientes.obtenerIterador();
+
+        System.out.println("PACIENTES DEL HOSPITAL: ");
+
+        while(I.hasNext()){
+            I.next().verPaciente();
+        }
     }
 }
